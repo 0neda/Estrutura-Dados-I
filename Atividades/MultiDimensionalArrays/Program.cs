f4fa58af414847d25b775c658e506bf3a439a165
0neda <alexoneda@live.com>
2023-09-11 19:44:04 -0300

    Adição da stack

--- Atividades/MultiDimensionalArrays/Program.cs
+++ Atividades/MultiDimensionalArrays/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int[,] biNumbers = new int[3, 3];
+
+//Vetor de três dimensões:
+//int[ , , ] triNumbers = new int[3, 3, 3];
+
+//Criar já carregada:
+int[,] loadedBiNumbers = new int[,] {
+    {1 ,2 ,3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+for(int i = 0; i < loadedBiNumbers.GetLength(0); i++)
+{
+    for (int j = 0; j < loadedBiNumbers.GetLength(1); j++)
+    {
+        Console.Write($"[{loadedBiNumbers[i, j]}]");
+    }
+    Console.WriteLine("");
+}
