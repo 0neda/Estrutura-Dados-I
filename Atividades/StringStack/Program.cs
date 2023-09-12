e2b8e04ddd58bd16d3f9ec9fc2d09baaa0bb06cf
Oneda / Gyzrak <alexoneda@live.com>
2023-09-11 21:44:14 -0300

    Adição de comentários

--- Atividades/StringStack/Program.cs
+++ Atividades/StringStack/Program.cs
@@ -11,24 +11,25 @@ while (running){
     Console.WriteLine("4- Clear");
     Console.WriteLine("5- Print");
     Console.WriteLine("6- Exit");
+    // Prints acima p/ ilustrar escolhas possíveis
 
-    string? inputX = Console.ReadLine();
-    if (inputX != null){
-        if (int.TryParse(inputX, out int selectedOption))
+    string? inputX = Console.ReadLine(); // string? possibilita null, armazena o input do usuário como inputX
+    if (inputX != null){ // se inputX for diferente de nulo
+        if (int.TryParse(inputX, out int selectedOption)) // converte inputX (até então string) em selectedOption (agora uma int)
         {
-            switch(selectedOption){
-                case 1:
+            switch(selectedOption){ // detecta a opção selecionada
+                case 1: // caso selectedOption seja 1
                     Console.WriteLine("Digite a string para adição:");
-                    string? inputY = Console.ReadLine();
-                    if (inputY != null){
-                        stringStack.Push(inputY);
+                    string? inputY = Console.ReadLine(); // string a ser adicionada salva em inputY
+                    if (inputY != null){ // se a inputY não for nula
+                        stringStack.Push(inputY); // faz o push da inputY
                     } else {
-                        Console.WriteLine("String nula e inválida");
+                        Console.WriteLine("String nula e inválida"); // se é nula, retorna erro
                     }
                     break;
                 case 2:
-                    string popped = stringStack.Pop();
-                    Console.WriteLine($"Valor removido: {popped}");
+                    string popped = stringStack.Pop(); // faz o pop e armazena o retorno em popped
+                    Console.WriteLine($"Valor removido: {popped}"); // printa valor do índice removido (armazenado em popped)
                     break;
                 case 3:
                     stringStack.Peek();
@@ -47,7 +48,7 @@ while (running){
                     break;
             }
         } else {
-            Console.WriteLine("Não é um número, porcão.");
+            Console.WriteLine("Não é um número, porcão."); // caso inputX não possa ser convertido em int (vide linha 18)
         }
     }
 }
